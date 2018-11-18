8294b630a4c67f5064c943ec6cd74340b93b69d8
Raman Plaha <ramanplaha@Ramans-MacBook-Pro-2.local>
2018-11-18 17:18:54 +0000

    installing copy webpack plugin to copy over data to dist folder for build

--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,61 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{ 
-				test: /\.(js)$/, 
-				use: 'babel-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: ['css-loader','sass-loader'],
-					publicPath: '/'
-				})
-			},
-			{
-				test: /\.(png|jpg|svg)$/,
-				use: [ 'file-loader' ],
-				exclude: /node_modules/
-			}
-		]
-	},
-	optimization: {
-		minimizer: [
-			new UglifyJsPlugin({
-				cache: true,
-				parallel: true,
-				sourceMap: true
-			})
-		]
-	},
-	mode: 'development',
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'src/index.html'
-		}),
-		new ExtractTextPlugin({
-			filename: 'bundle.css',
-			allChunks: false,
-			disable: false,
-		})
-	]
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(js)$/, 
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader','sass-loader'],
+          publicPath: '/'
+        })
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: [ 'file-loader' ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      })
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      allChunks: false,
+      disable: false,
+    }),
+    new CopyWebpackPlugin(
+      [{
+        from: 'data/feed.json', 
+        to: 'data'
+      }]
+    ),
+  ]
 };
